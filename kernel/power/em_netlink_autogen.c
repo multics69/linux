77c9521ed3a483c1c39d8aa8cb2f7fae5d8766d6
Changwoo Min <changwoo@igalia.com>
2025-12-18 14:42:34 +0900

    XXX: PM: EM: Update the autogen files
    Signed-off-by: Changwoo Min <changwoo@igalia.com>

--- kernel/power/em_netlink_autogen.c
+++ kernel/power/em_netlink_autogen.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: ((GPL-2.0 WITH Linux-syscall-note) OR BSD-3-Clause)
 /* Do not edit directly, auto-generated from: */
-/*	Documentation/netlink/specs/em.yaml */
+/*	Documentation/netlink/specs/dev-energy-model.yaml */
 /* YNL-GEN kernel source */
 /* To regenerate run: tools/net/ynl/ynl-regen.sh */
 
@@ -9,41 +9,41 @@
 
 #include "em_netlink_autogen.h"
 
-#include <uapi/linux/energy_model.h>
+#include <uapi/linux/dev_energy_model.h>
 
-/* EM_CMD_GET_PD_TABLE - do */
-static const struct nla_policy em_get_pd_table_nl_policy[EM_A_PD_TABLE_PD_ID + 1] = {
-	[EM_A_PD_TABLE_PD_ID] = { .type = NLA_U32, },
+/* DEV_ENERGY_MODEL_CMD_GET_PERF_TABLE - do */
+static const struct nla_policy dev_energy_model_get_perf_table_nl_policy[DEV_ENERGY_MODEL_A_PERF_TABLE_PERF_DOMAIN_ID + 1] = {
+	[DEV_ENERGY_MODEL_A_PERF_TABLE_PERF_DOMAIN_ID] = { .type = NLA_U32, },
 };
 
-/* Ops table for em */
-static const struct genl_split_ops em_nl_ops[] = {
+/* Ops table for dev_energy_model */
+static const struct genl_split_ops dev_energy_model_nl_ops[] = {
 	{
-		.cmd	= EM_CMD_GET_PDS,
-		.doit	= em_nl_get_pds_doit,
+		.cmd	= DEV_ENERGY_MODEL_CMD_GET_PERF_DOMAINS,
+		.doit	= dev_energy_model_nl_get_perf_domains_doit,
 		.flags	= GENL_CMD_CAP_DO,
 	},
 	{
-		.cmd		= EM_CMD_GET_PD_TABLE,
-		.doit		= em_nl_get_pd_table_doit,
-		.policy		= em_get_pd_table_nl_policy,
-		.maxattr	= EM_A_PD_TABLE_PD_ID,
+		.cmd		= DEV_ENERGY_MODEL_CMD_GET_PERF_TABLE,
+		.doit		= dev_energy_model_nl_get_perf_table_doit,
+		.policy		= dev_energy_model_get_perf_table_nl_policy,
+		.maxattr	= DEV_ENERGY_MODEL_A_PERF_TABLE_PERF_DOMAIN_ID,
 		.flags		= GENL_CMD_CAP_DO,
 	},
 };
 
-static const struct genl_multicast_group em_nl_mcgrps[] = {
-	[EM_NLGRP_EVENT] = { "event", },
+static const struct genl_multicast_group dev_energy_model_nl_mcgrps[] = {
+	[DEV_ENERGY_MODEL_NLGRP_EVENT] = { "event", },
 };
 
-struct genl_family em_nl_family __ro_after_init = {
-	.name		= EM_FAMILY_NAME,
-	.version	= EM_FAMILY_VERSION,
+struct genl_family dev_energy_model_nl_family __ro_after_init = {
+	.name		= DEV_ENERGY_MODEL_FAMILY_NAME,
+	.version	= DEV_ENERGY_MODEL_FAMILY_VERSION,
 	.netnsok	= true,
 	.parallel_ops	= true,
 	.module		= THIS_MODULE,
-	.split_ops	= em_nl_ops,
-	.n_split_ops	= ARRAY_SIZE(em_nl_ops),
-	.mcgrps		= em_nl_mcgrps,
-	.n_mcgrps	= ARRAY_SIZE(em_nl_mcgrps),
+	.split_ops	= dev_energy_model_nl_ops,
+	.n_split_ops	= ARRAY_SIZE(dev_energy_model_nl_ops),
+	.mcgrps		= dev_energy_model_nl_mcgrps,
+	.n_mcgrps	= ARRAY_SIZE(dev_energy_model_nl_mcgrps),
 };
