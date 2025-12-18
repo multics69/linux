fdd54da9069c749e37679d97f2161b4da7637665
Changwoo Min <changwoo@igalia.com>
2025-12-18 11:23:40 +0900

    XXX: PM: EM: Add autogenerated files
    	new file:   ../../include/uapi/linux/device_energy_model.h
    	deleted:    ../../include/uapi/linux/energy_model.h
    	modified:   em_netlink_autogen.c
    	modified:   em_netlink_autogen.h
    
    Signed-off-by: Changwoo Min <changwoo@igalia.com>

--- kernel/power/em_netlink_autogen.c
+++ kernel/power/em_netlink_autogen.c
@@ -1,48 +1,61 @@
 // SPDX-License-Identifier: ((GPL-2.0 WITH Linux-syscall-note) OR BSD-3-Clause)
 /* Do not edit directly, auto-generated from: */
-/*	Documentation/netlink/specs/em.yaml */
+/*	Documentation/netlink/specs/device-energy-model.yaml */
 /* YNL-GEN kernel source */
+/* To regenerate run: tools/net/ynl/ynl-regen.sh */
 
 #include <net/netlink.h>
 #include <net/genetlink.h>
 
 #include "em_netlink_autogen.h"
 
-#include <uapi/linux/energy_model.h>
+#include <uapi/linux/device_energy_model.h>
 
-/* EM_CMD_GET_PD_TABLE - do */
-static const struct nla_policy em_get_pd_table_nl_policy[EM_A_PD_TABLE_PD_ID + 1] = {
-	[EM_A_PD_TABLE_PD_ID] = { .type = NLA_U32, },
+/* DEVICE_ENERGY_MODEL_CMD_GET_PERF_DOMAINS - do */
+static const struct nla_policy device_energy_model_get_perf_domains_nl_policy[DEVICE_ENERGY_MODEL_A_PERF_DOMAINS_PERF_DOMAIN_ID + 1] = {
+	[DEVICE_ENERGY_MODEL_A_PERF_DOMAINS_PERF_DOMAIN_ID] = { .type = NLA_U32, },
 };
 
-/* Ops table for em */
-static const struct genl_split_ops em_nl_ops[] = {
+/* DEVICE_ENERGY_MODEL_CMD_GET_PERF_TABLE - do */
+static const struct nla_policy device_energy_model_get_perf_table_nl_policy[DEVICE_ENERGY_MODEL_A_PERF_TABLE_PERF_DOMAIN_ID + 1] = {
+	[DEVICE_ENERGY_MODEL_A_PERF_TABLE_PERF_DOMAIN_ID] = { .type = NLA_U32, },
+};
+
+/* Ops table for device_energy_model */
+static const struct genl_split_ops device_energy_model_nl_ops[] = {
 	{
-		.cmd	= EM_CMD_GET_PDS,
-		.doit	= em_nl_get_pds_doit,
-		.flags	= GENL_CMD_CAP_DO,
+		.cmd		= DEVICE_ENERGY_MODEL_CMD_GET_PERF_DOMAINS,
+		.doit		= device_energy_model_nl_get_perf_domains_doit,
+		.policy		= device_energy_model_get_perf_domains_nl_policy,
+		.maxattr	= DEVICE_ENERGY_MODEL_A_PERF_DOMAINS_PERF_DOMAIN_ID,
+		.flags		= GENL_CMD_CAP_DO,
 	},
 	{
-		.cmd		= EM_CMD_GET_PD_TABLE,
-		.doit		= em_nl_get_pd_table_doit,
-		.policy		= em_get_pd_table_nl_policy,
-		.maxattr	= EM_A_PD_TABLE_PD_ID,
+		.cmd	= DEVICE_ENERGY_MODEL_CMD_GET_PERF_DOMAINS,
+		.dumpit	= device_energy_model_nl_get_perf_domains_dumpit,
+		.flags	= GENL_CMD_CAP_DUMP,
+	},
+	{
+		.cmd		= DEVICE_ENERGY_MODEL_CMD_GET_PERF_TABLE,
+		.doit		= device_energy_model_nl_get_perf_table_doit,
+		.policy		= device_energy_model_get_perf_table_nl_policy,
+		.maxattr	= DEVICE_ENERGY_MODEL_A_PERF_TABLE_PERF_DOMAIN_ID,
 		.flags		= GENL_CMD_CAP_DO,
 	},
 };
 
-static const struct genl_multicast_group em_nl_mcgrps[] = {
-	[EM_NLGRP_EVENT] = { "event", },
+static const struct genl_multicast_group device_energy_model_nl_mcgrps[] = {
+	[DEVICE_ENERGY_MODEL_NLGRP_EVENT] = { "event", },
 };
 
-struct genl_family em_nl_family __ro_after_init = {
-	.name		= EM_FAMILY_NAME,
-	.version	= EM_FAMILY_VERSION,
+struct genl_family device_energy_model_nl_family __ro_after_init = {
+	.name		= DEVICE_ENERGY_MODEL_FAMILY_NAME,
+	.version	= DEVICE_ENERGY_MODEL_FAMILY_VERSION,
 	.netnsok	= true,
 	.parallel_ops	= true,
 	.module		= THIS_MODULE,
-	.split_ops	= em_nl_ops,
-	.n_split_ops	= ARRAY_SIZE(em_nl_ops),
-	.mcgrps		= em_nl_mcgrps,
-	.n_mcgrps	= ARRAY_SIZE(em_nl_mcgrps),
+	.split_ops	= device_energy_model_nl_ops,
+	.n_split_ops	= ARRAY_SIZE(device_energy_model_nl_ops),
+	.mcgrps		= device_energy_model_nl_mcgrps,
+	.n_mcgrps	= ARRAY_SIZE(device_energy_model_nl_mcgrps),
 };
